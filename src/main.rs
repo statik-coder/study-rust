953d06eaaed70799e9115a6f3328c1ed89ec3b4a
Artjoms S <artjoms.s@develops.today>
2021-12-26 03:23:42 +0200

    learnt 6.3 book point

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     // is that tuple can store values with different types
     let tuple: (&str, u8, char) = ("Some value", 65, '🌅');
 
-    // There I destructure values from tuple
+    // There I de structure values from tuple
     let (string, number, char) = tuple;
 
     // Also can access to values with dot (.)
@@ -91,6 +91,8 @@ fn main() {
 
     separate("Structs");
 
+    // derive(Debug) - this some kind of prefix implement Debug method (pretty output)
+    #[derive(Debug)]
     struct Player {
         name: String,
         username: String,
@@ -98,10 +100,98 @@ fn main() {
         skill_level: String
     }
 
-    let player = Player {
+    impl Player {
+        fn say_hi(&self) -> () {
+            println!("User {} says: Hi!", self.username)
+        }
+
+        fn send_message(&self, receiver: &Player, message: &str) -> () {
+            println!(
+                "User {} sends message to {}: {}",
+                self.username,
+                receiver.username,
+                message
+            )
+        }
+    }
+
+    let player1 = Player {
         name: String::from("Artem"),
         username: String::from("stat1k"),
         age: 19,
-        skill_level: String::from("")
+        skill_level: String::from("High++")
+    };
+
+    let player2 = Player {
+        name: String::from("Oleg"),
+        username: String::from("oleg-33"),
+        age: 20,
+        skill_level:String::from("High++")
+    };
+
+    player1.say_hi();
+    player2.send_message(&player1, "HI, mate! What's going?");
+
+
+    // To print objects in console we should in braces type "{:#?}"
+    // use debug syntax
+    println!("This is the first player: {:#?}", &player1);
+
+    separate("Enums");
+
+    #[derive(Debug)]
+    enum IPVersion {
+        V4,
+        V6
+    }
+
+    struct IPAddress {
+        version: IPVersion,
+        value: String
+    }
+
+    let home_addr = IPAddress {
+        version: IPVersion::V4,
+        value: String::from("192.168.0.1")
+    };
+
+    println!("My home address is {} (version: {:?})", &home_addr.value, &home_addr.version);
+
+    // Also possible to create instances with associated functions
+    // That functions implements in types and calls with this syntax - TYPE::AF
+    // For example - String::from()
+    // Let's create own function in IPAddress struct
+    //
+    // AF - associated functions
+
+    impl IPAddress {
+        fn create_v4(value: &str) -> IPAddress {
+            IPAddress {
+                version: IPVersion::V4,
+                value: String::from(value)
+            }
+        }
+    }
+
+    let my_v4_ip_address = IPAddress::create_v4("127.0.0.1");
+    println!("My local ip v4 address is {}", my_v4_ip_address.value);
+
+    // There is one more opportunity how to create struct instance from "enum creators"
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(String),
+        V6(String)
+    };
+
+    let v4addr = IpAddr::V4(String::from("localhost"));
+    println!("Device local host is - {:#?}", &v4addr);
+
+    let some_num: Option<i32> = Some(5);
+
+    match some_num {
+        None => {
+            println!("There is no value")
+        }
+        Some(num) => println!("The value is: {}", num)
     }
 }
